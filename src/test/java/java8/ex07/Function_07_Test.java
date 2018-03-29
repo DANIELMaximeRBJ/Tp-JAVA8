402476205900f34ac96dce7e605ed5ae07e2d76e
Maxime.DANIEL <m.daniel2009@hotmail.fr>
2018-03-29 09:38:50 +0200

    ajout opérator

--- src/test/java/java8/ex07/Function_07_Test.java
+++ src/test/java/java8/ex07/Function_07_Test.java
@@ -13,13 +13,16 @@ public class Function_07_Test {
     // TODO compléter la méthode pour qu'elle renvoie une chaîne de caractères de la forme "(<nb1><symbol><nb2>)=<resultat>"
     // TODO ex. "(10+11)=21", "(5-2)=3"
     String format(int nb1, int nb2, String symbol, IntBinaryOperator operator) {
+    	String result = "";
+    	result = "(" + nb1 + symbol + nb2 + ")=" + operator.applyAsInt(nb1, nb2);
         // TODO
-        return null;
+        return result;
     }
     // end::format[]
 
     // TODO définir sum pour que le test test_format_sum() soit passant
-    IntBinaryOperator sum = null;
+    IntBinaryOperator sum = (i1,i2) -> i1+i2;
+
 
     @Test
     public void test_format_sum() throws Exception {
@@ -30,7 +33,8 @@ public class Function_07_Test {
     }
 
     // TODO définir substract afin que le test test_format_subtract() soit passant
-    IntBinaryOperator substract = null;
+    
+    IntBinaryOperator substract = (i1,i2) -> i1-i2;
 
     @Test
     public void test_format_subtract() throws Exception {
